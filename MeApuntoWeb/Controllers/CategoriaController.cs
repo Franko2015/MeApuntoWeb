7d7a4a7739defa27f09cd4b3817e8bf8f4a43951
Franco Alberto Millanes Araya <franko20155@gmail.com>
2022-06-03 22:09:26 -0400

    catController

---
+++ MeApuntoWeb/Controllers/CategoriaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeApuntoWeb.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
