955aa66d183b454c65bc9daad74c165c9ee87807
FRANCO ALBERTO MILLANES ARAYA <Franko20155@gmail.com>
2022-06-06 03:19:04 -0400

    login y CreateCategoria

--- MeApuntoWeb/Controllers/CategoriaController.cs
+++ MeApuntoWeb/Controllers/CategoriaController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MeApuntoWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MeApuntoWeb.Controllers
 {
     public class CategoriaController : Controller
     {
+        private readonly EventosDbContext _context;
+        public CategoriaController(EventosDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var Categorias = _context.tblCategoria.ToList();
+            return View(Categorias);
+        }
+        public IActionResult Add()
+        {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(Categoria c)
+        {
+            if(c == null) return View();
+
+            _context.Add(c);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
