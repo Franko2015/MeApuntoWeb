3d1b57aa3d7bd89b4cda21b38215ccc67d0db6f5
FRANCO ALBERTO MILLANES ARAYA <Franko20155@gmail.com>
2022-06-26 20:29:19 -0400

    Revert "cambio createusuario miseventos"
    This reverts commit c4da9c46718fc20cd4a45fb4eea2990e47bf4dc8.

--- MeApuntoWeb/Models/Categoria.cs
+++ MeApuntoWeb/Models/Categoria.cs
@@ -4,7 +4,7 @@
     public class Categoria
     {
         public int Id { get; set; }
-        public string? categoria { get; set; }
-        public string? imagen { get; set; }
+        public string categoria { get; set; }
+        public string imagen { get; set; }
     }
 }
