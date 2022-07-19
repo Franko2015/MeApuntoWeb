d3137161efe7a521c61ac182061ba6a644a6f34c
JessyMarilyn22 <Jessy.alvarez@inacapmail.cl>
2022-07-18 21:44:22 -0400

    .

--- MeApuntoWeb/Controllers/HomeController.cs
+++ MeApuntoWeb/Controllers/HomeController.cs
@@ -75,6 +75,8 @@ namespace MeApuntoWeb.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+       
+    
 
     }
 }
