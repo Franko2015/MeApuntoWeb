33c4f7d342230725be374aeebc36e14b1fcaef01
Franco Alberto Millanes Araya <franko20155@gmail.com>
2022-07-25 17:42:19 -0400

    Notificaciones

---
+++ MeApuntoWeb/ViewModels/EventosNotificacionViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MeApuntoWeb.Models;
+
+namespace MeApuntoWeb.ViewModels
+{
+    public class EventosNotificacionViewModel
+    {
+        //Llaves foráneas
+        public int CategoriaId { get; set; }
+        public int UsuarioId { get; set; }
+        public int NotificacionId { get; set; }
+        public int EventoId { get; set; }
+
+
+        //Referencias a otras clases
+        public Categoria? Categoria { get; set; }
+        public Evento? Evento { get; set; }
+        public Usuario? Usuario { get; set; }
+        public Notificaciones Notificacion { get; set; }
+    }
+}
---
+++ MeApuntoWeb/ViewModels/NotificacionesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MeApuntoWeb.Models;
+
+namespace MeApuntoWeb.ViewModels
+{
+    public class NotificacionesViewModel
+    {
+        public int Id { get; set; }
+        public string Notificacion { get; set; }
+        public int UsuarioReceptor { get; set; }
+        public int UsuarioRemitente { get; set; }
+
+        public Usuario Usuario { get; set; }
+    }
+}
